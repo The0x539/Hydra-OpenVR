e70841a79a212c97efba5cdea609454a67ed4455
The0x539 <the0x539@gmail.com>
2022-10-10 17:13:31 -0500

    Use sane button bindings for Index controllers

--- main.cpp
+++ main.cpp
@@ -105,29 +105,17 @@ void sixenseThreadFunc()
             data.trigger = state.rAxis[1].x;
 #endif
 
-            if (state.ulButtonPressed & vr::ButtonMaskFromId(vr::k_EButton_Grip))
+            if (state.ulButtonPressed & vr::ButtonMaskFromId(vr::k_EButton_SteamVR_Touchpad))
                 data.buttons |= SIXENSE_BUTTON_BUMPER;
 
-            if (state.ulButtonPressed & vr::ButtonMaskFromId(vr::k_EButton_ApplicationMenu))
-                data.buttons |= SIXENSE_BUTTON_START;
+            if (state.ulButtonPressed & vr::ButtonMaskFromId(vr::k_EButton_Grip))
+                data.buttons |= SIXENSE_BUTTON_1;
 
-            if (state.ulButtonPressed & vr::ButtonMaskFromId(vr::k_EButton_SteamVR_Touchpad))
-            {
-                if (state.rAxis[0].y < 0.0f)
-                {
-                    if (state.rAxis[0].x < 0.0f)
-                        data.buttons |= SIXENSE_BUTTON_1;
-                    else
-                        data.buttons |= SIXENSE_BUTTON_2;
-                }
-                else
-                {
-                    if (state.rAxis[0].x < 0.0f)
-                        data.buttons |= SIXENSE_BUTTON_3;
-                    else
-                        data.buttons |= SIXENSE_BUTTON_4;
-                }
-            }
+            if (state.ulButtonPressed & vr::ButtonMaskFromId(vr::k_EButton_ApplicationMenu))
+                data.buttons |= SIXENSE_BUTTON_3;
+
+            if (state.ulButtonPressed & 35)
+                data.buttons |= SIXENSE_BUTTON_START;
 
             data.sequence_number = sequence;
 
